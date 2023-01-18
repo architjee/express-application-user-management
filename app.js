1e1241e0066447f9ff48f1e1a77a6bb7851e222c
Archit Jain <32292295+architjee@users.noreply.github.com>
2023-01-18 09:18:34 +0530

    working database and express application

--- app.js
+++ app.js
@@ -1,41 +1,127 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// Create express app
+var express = require("express")
+var db = require("./database.js")
+var app = express()
+var md5 = require("md5")
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Server port
+var HTTP_PORT = 8000 
+// Start server
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+// Root endpoint
+app.get("/", (req, res, next) => {
+    res.json({"message":"Ok"})
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get("/api/user/:id", (req, res, next) => {
+  var sql = "select * from user where id = ?"
+  var params = [req.params.id]
+  db.get(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      res.json({
+          "message":"success",
+          "data":row
+      })
+    });
 });
 
-module.exports = app;
+// Insert here other API endpoints
+app.get("/api/users", (req, res, next) => {
+  var sql = "select * from user"
+  var params = []
+  db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      res.json({
+          "message":"success",
+          "data":rows
+      })
+    });
+});
+
+app.post("/api/user/", (req, res, next) => {
+  var errors=[]
+  if (!req.body.password){
+      errors.push("No password specified");
+  }
+  if (!req.body.email){
+      errors.push("No email specified");
+  }
+  if (errors.length){
+      res.status(400).json({"error":errors.join(",")});
+      return;
+  }
+  var data = {
+      name: req.body.name,
+      email: req.body.email,
+      password : md5(req.body.password)
+  }
+  var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
+  var params =[data.name, data.email, data.password]
+  db.run(sql, params, function (err, result) {
+      if (err){
+          res.status(400).json({"error": err.message})
+          return;
+      }
+      res.json({
+          "message": "success",
+          "data": data,
+          "id" : this.lastID
+      })
+  });
+})
+app.patch("/api/user/:id", (req, res, next) => {
+  var data = {
+      name: req.body.name,
+      email: req.body.email,
+      password : req.body.password ? md5(req.body.password) : null
+  }
+  db.run(
+      `UPDATE user set 
+         name = COALESCE(?,name), 
+         email = COALESCE(?,email), 
+         password = COALESCE(?,password) 
+         WHERE id = ?`,
+      [data.name, data.email, data.password, req.params.id],
+      function (err, result) {
+          if (err){
+              res.status(400).json({"error": res.message})
+              return;
+          }
+          res.json({
+              message: "success",
+              data: data,
+              changes: this.changes
+          })
+  });
+})
+app.delete("/api/user/:id", (req, res, next) => {
+  db.run(
+      'DELETE FROM user WHERE id = ?',
+      req.params.id,
+      function (err, result) {
+          if (err){
+              res.status(400).json({"error": res.message})
+              return;
+          }
+          res.json({"message":"deleted", changes: this.changes})
+  });
+})
+
+
+
+// Default response for any other request
+app.use(function(req, res){
+    res.status(404);
+});
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+router.get("/api/users", (req, res, next) => {
+  var sql = "select * from user"
+  var params = []
+  db.all(sql, params, (err, rows) => {
+      if (err) {
+        res.status(400).json({"error":err.message});
+        return;
+      }
+      res.json({
+          "message":"success",
+          "data":rows
+      })
+    });
+});
+
+
 
 module.exports = router;
